39a5d0255f38216a5753f1491d7deabfb42bcd3a
pavan <kavvuripavankumar@gmail.com>
2016-07-27 22:38:13 +0530

    ok

---
+++ app/src/main/java/pavan/com/trialactivity/MainActivity.java
@@ -0,0 +1,25 @@
+package pavan.com.trialactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+}
